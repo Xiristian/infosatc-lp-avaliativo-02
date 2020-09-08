052d021807254df6345338a2bad4342963a726a9
Xiristian <christianhederson22@gmail.com>
2020-09-08 12:41:47 -0300

    feat: Atividade 07

---
+++ atividade07.py
@@ -0,0 +1,8 @@
+PecasParaPC = ["RTX2080", "Gabinete Cooler Master", "Processador i9", "Placa Mãe Gigabyte B450M", "Monitor 27' Gamemax", "SSD Kingston A400 240GB"]
+
+print("Carrinho de compras: ", PecasParaPC)
+
+PecasParaPC.remove("RTX2080")
+PecasParaPC.remove("Gabinete Cooler Master")
+
+print("Carrinho de compras com itens removidos: ", PecasParaPC)
