eb3035f33a162147e1742ab8289ab662255d7530
Xiristian <christianhederson22@gmail.com>
2020-09-08 12:40:55 -0300

    feat: Atividade 05

---
+++ atividade05.py
@@ -0,0 +1,8 @@
+minhalista = [3, 5, 6, 4, 9]
+
+elementoX = int(input("Digite um número inteiro para saber se o item esta na lista: "))
+
+if elementoX in minhalista:
+    print("O item digitado está na lista")
+else:
+    print("O item digitado não está na lista")
